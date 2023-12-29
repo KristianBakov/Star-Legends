81d0ab1f25fd911e946961e95c563ff6101147f3
KristianBakov <kristian.bakov@yahoo.com>
2023-12-29 16:23:27 +0000

    Added agent data scriptable objects for UI and agent select button styling

---
+++ Assets/Scripts/UI/AgentData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+public enum AgentType
+{
+    Duelist,
+    Initiator,
+    Sentinel,
+    Controller
+}
+
+[CreateAssetMenu(fileName = "AgentData", menuName = "ScriptableObjects/AgentData", order = 1)]
+public class AgentData : ScriptableObject
+{
+    public string agentName;
+    public AgentType agentType;
+    public Sprite agentImage;
+    public string agentDescription;
+    //public string agentAbilities;
+}
